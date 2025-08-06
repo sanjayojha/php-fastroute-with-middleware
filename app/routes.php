684f8a8c66316ab1a85836c5a5a4361c5ab613cc
Sanjay Ojha <sanjayojha@users.noreply.github.com>
2025-08-06 11:59:13 +0530

    Update routes.php

--- app/routes.php
+++ app/routes.php
@@ -7,6 +7,7 @@ use App\Controllers\AdminController;
 use App\Middleware\AuthMiddleware;
 use App\Middleware\RoleMiddleware;
 use App\Routing\RouteDefinition;
+use App\Services\SessionManager;
 use FastRoute\RouteCollector;
 use function FastRoute\simpleDispatcher;
 
@@ -19,10 +20,10 @@ $routes = [
     
     // Routes requiring authentication
     (new RouteDefinition('GET', '/dashboard', [DashboardController::class, 'index']))
-        ->middleware(new AuthMiddleware()),
+        ->middleware(new AuthMiddleware(new SessionManager)),
     
     (new RouteDefinition('GET', '/profile', [UserController::class, 'profile']))
-        ->middleware(new AuthMiddleware()),
+        ->middleware(new AuthMiddleware(new SessionManager)),
     
     (new RouteDefinition('GET', '/logout', [UserController::class, 'logout']))
         ->middleware(new AuthMiddleware()),
@@ -30,21 +31,21 @@ $routes = [
     // Admin-only routes
     (new RouteDefinition('GET', '/admin', [AdminController::class, 'index']))
         ->middlewares([
-            new AuthMiddleware(),
-            new RoleMiddleware(['admin'])
+            new AuthMiddleware(new SessionManager),
+            new RoleMiddleware(new SessionManager, ['admin'])
         ]),
     
     (new RouteDefinition('GET', '/admin/users', [AdminController::class, 'users']))
         ->middlewares([
-            new AuthMiddleware(),
-            new RoleMiddleware(['admin'])
+            new AuthMiddleware(new SessionManager),
+            new RoleMiddleware(new SessionManager, ['admin'])
         ]),
 
     // Routes for multiple roles
     (new RouteDefinition('GET', '/moderator', [DashboardController::class, 'moderator']))
         ->middlewares([
             new AuthMiddleware(),
-            new RoleMiddleware(['admin', 'moderator'])
+            new RoleMiddleware(new SessionManager, ['admin', 'moderator'])
         ]),
 ];
 
